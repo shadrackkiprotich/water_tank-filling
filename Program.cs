c8ff2ea3cdccd7751209fdcf2bb467bf3d07e965
shadrackkiprotich <mylesshadrack99@gmail.com>
2019-01-17 21:54:22 -0800

    water_tank_filling

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿    `1using System;
+
+namespace Water_Tank_Filling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("2 litres and \n "+ "3 litres buckets");
+        Console.WriteLine("Enter the volume of the tank");
+        
+        int t=Convert.ToInt32(Console.ReadLine());
+        if (t< 10 || t> 9999)
+        {
+           Console.WriteLine("You have entered an invalid entry");
+           
+        }
+        else
+        {
+             int j=5;
+        int k=t/j-2;
+        Console.WriteLine($"Both buckets can fill the tank {k} times");
+            
+        }
+       
+
+
+        //Console.ReadKey;
+        }
+    }
+}
